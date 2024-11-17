fc2331609c0719853dc7e36d15b585b0e5d6cc1d
Manan Garg <manangarg1929@gmail.com>
2024-11-18 04:41:29 +0530

    Create layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,32 @@
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+// Metadata for the page, including title and description
+export const metadata = {
+  title: "Create Next App",
+  description: "GoQuant's Crypto Currency OrderBook and Charts",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {/* Rendering the children components passed to this layout */}
+        {children}
+      </body>
+    </html>
+  );
+}
